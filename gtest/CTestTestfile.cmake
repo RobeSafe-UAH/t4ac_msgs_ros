bc750a5d685f93defb1d801de85c226f18861edb
Javier-DlaP <javirachi99@hotmail.es>
2022-01-25 17:07:14 +0100

    Chaos solved

--- gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/robesafe/t4ac_ws/src/t4ac_architecture/t4ac_config_layer/t4ac_msgs_ros/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/robesafe/t4ac_ws/src/t4ac_architecture/t4ac_config_layer/t4ac_msgs_ros/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
